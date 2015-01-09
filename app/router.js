b7f9514f2ac5b91966b8bebbb22397b1ad649cf6
Dave Choi <daaave@gmail.com>
2015-01-09 10:21:23 -0500

    Add routes to resources.
    Singular resources are covered implicitly.

--- app/router.js
+++ app/router.js
@@ -1,11 +1,29 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
+  this.resource("quests", function() {
+    this.resource("quest", {
+      path: ":quest_id"
+    });
+  });
+
+  this.resource("lords", function() {
+    this.resource("lord", {
+      path: ":lord_id"
+    });
+  });
+
+  this.resource("intrigues", function() {
+  	this.resource("intrigue", { path: ":intrigue_id" });
+  });
+  this.resource("buildings", function() {
+  	this.resource("building", { path: ":building_id" });
+  });
 });
 
 export default Router;
